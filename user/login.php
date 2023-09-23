7a46fa5720c922296486b4dcab2604f4c6d8104f
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-23 15:26:40 +0900

    make form login and createaccount don't have funcrion

--- user/login.php
+++ user/login.php
@@ -8,13 +8,26 @@
 </head>
 
 <body>
-    <a href="./connectDB.php">
-        <button>connectDB</button>
-    </a>
-    <a href="./search.php">
-        <button>searchHome</button>
-    </a>
+    <?php
+    require_once("./util/connectDB.php");
+    $pdo = connect();
+    ?>
 
+    <form action="" method="post">
+        <input type="text" placeholder="ユーザ名" name="userName"><br>
+        <input type="password" placeholder="パスワード" name="password"><br>
+        <input type="submit" value="ログイン" name="login"><br>
+        <input type="submit" value="アカウント新規作成" name="create">
+    </form>
+    <?php
+    if (!empty($_POST['login'])) {
+        header("Location: ../ezTimeStamp/search.php");
+    }
+    if (!empty($_POST['create'])) {
+        header("Location: ./create.php");
+    }
+
+    ?>
 </body>
 
 </html>
