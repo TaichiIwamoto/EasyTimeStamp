0ee2263b42d888613ccbead5b3890737c4c7b3ae
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-23 15:26:10 +0900

    change connectDB to function return pdo

--- util/connectDB.php
+++ util/connectDB.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <?php
+<?php
+function connect()
+{
     require_once('./vendor/autoload.php');
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
@@ -22,34 +14,10 @@
     try {
         $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
         $pdo = new PDO($dsn, $username, $password);
-        echo "connected!";
-        // MySQLに接続成功
     } catch (PDOException $e) {
         exit('MySQL接続エラー: ' . $e->getMessage());
     }
 
-    $sql = "CREATE TABLE IF NOT EXISTS Test"
-        . "("
-        . "id INT AUTO_INCREMENT PRIMARY KEY,"
-        . "name char(32),"
-        . "data DATETIME,"
-        . "comment TEXT,"
-        . "resourceName TEXT"
-        . ");";
-    $stmt = $pdo->query($sql);
-
-    $sql = "SHOW TABLES";
-    $result = $pdo->query($sql);
-    if ($result) {
-        echo "テーブル一覧:<br>";
-        while ($row = $result->fetch(PDO::FETCH_NUM)) {
-            echo $row[0] . "<br>";
-        }
-    } else {
-        echo "テーブル情報の取得に失敗しました。";
-    }
-
-    ?>
-</body>
-
-</html>
+    return $pdo;
+}
+?>
